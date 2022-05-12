117aad02291f915b19d2ed7d48939f41af97ab91
Ruslan Khaziev <r.smim@yandex.ru>
2022-05-13 01:14:22 +0400

    1) Temporarly removed routesStrict

--- frontend/middleware/router-main.global.ts
+++ frontend/middleware/router-main.global.ts
@@ -4,18 +4,18 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const $userStore = useUserStore()
   
   
-  if (!$userStore.getUserInfo.existType && to.name !== 'index') {
-    return navigateTo('/')
-  }
-  
-  if (
-    $userStore.getUserInfo.existType &&
-    to.name !== 'index' &&
-    to.name !== $userStore.getUserInfo.existType
-  
-  ) {
-    return navigateTo('/')
-  }
+  // if (!$userStore.getUserInfo.existType && to.name !== 'index') {
+  //   return navigateTo('/')
+  // }
+  //
+  // if (
+  //   $userStore.getUserInfo.existType &&
+  //   to.name !== 'index' &&
+  //   to.name !== $userStore.getUserInfo.existType
+  //
+  // ) {
+  //   return navigateTo('/')
+  // }
   
 })
 
