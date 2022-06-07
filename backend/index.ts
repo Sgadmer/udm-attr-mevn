b928d3319f7e2e78d81188a33841a4e31e0742bf
Руслан Хазиев <ruslan.hasiev@nutnet.ru>
2022-06-07 16:45:44 +0400

    added cors in  backend config 6

--- backend/index.ts
+++ backend/index.ts
@@ -7,8 +7,14 @@ import AppRoutes from './routes'
 connectDB()
 
 const app = express()
-app.use(cors({credentials: true, origin: true}))
-// app.options('*', cors({credentials: true, origin: true}))
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(express.json())
