f2a892c7c96682e072599c0c7e024558c0dda571
Руслан Хазиев <ruslan.hasiev@nutnet.ru>
2022-06-07 16:59:34 +0400

    added cors in  backend config 8

--- backend/index.ts
+++ backend/index.ts
@@ -7,14 +7,8 @@ import AppRoutes from './routes'
 connectDB()
 
 const app = express()
-// enable cors to the server
-const corsOpt = {
-  origin: process.env.CORS_ALLOW_ORIGIN || '*', // this work well to configure origin url in the server
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
-  allowedHeaders: ['Content-Type', 'Authorization'] // allow json and token in the headers
-};
-app.use(cors(corsOpt)); // cors for all the routes of the application
-app.options('*', cors(corsOpt)); // automatic cors gen for HTTP verbs in all routes, This can be redundant but I kept to be sure that will always work.
+app.use(cors({credentials: true, origin: true}))
+app.options('*', cors({credentials: true, origin: true}))
 
 
 app.use(express.json())
