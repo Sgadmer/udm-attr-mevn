625bda02b3151af2c7ec97c957242d62ffe49839
Ruslan Khaziev <r.smim@yandex.ru>
2022-06-15 02:58:08 +0400

    Hotfix 1 1) disabled cookies user store key encryption because it create it differently at every page reload

--- frontend/constants/storeNames.ts
+++ frontend/constants/storeNames.ts
@@ -1,15 +1,5 @@
 import CryptoJS from 'crypto-js'
 
-const storeNames = {
+export const STORE_NAMES = {
   USER: 'USER',
 }
-
-const handleStoryName = {
-  get(target, name) {
-    if (process.env.NODE_ENV === 'production') {
-      return CryptoJS.AES.encrypt(target[name], 'C!IqxTy2P70JrMF`')
-    } else return target[name]
-  }
-}
-
-export const STORE_NAMES = new Proxy(storeNames, handleStoryName)
